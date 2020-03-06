9ae4fc9252008c696082f20fdbdf7cf663130c70
Rammireddy7 <61828232+Rammireddy7@users.noreply.github.com>
2020-03-06 09:03:23 +0530

    Add files via upload

---
+++ ram2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c,d,e,f;
+printf("enter a and b values");
+scanf("%d%d",&a,&b);
+c=a+b;
+d=a-b;
+e=a*b;
+f=a/b;
+printf("addition=%d",c);
+printf("substraction=%d",a);
+printf("multiply=%d",e);
+printf("division=%d",f);
+}
