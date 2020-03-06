a5dfe020d83bb3eb16f5bd04d47b3c59031918c5
Rammireddy7 <61828232+Rammireddy7@users.noreply.github.com>
2020-03-06 08:53:53 +0530

    Add files via upload

---
+++ ram1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int s1,s2,s3,s4,s5,t,avg;
+printf("enter 5 subjects marks");
+scanf("%d%d%d%d%d",&s1,&s2,&s3,&s4,&s5);
+t=s1+s2+s3+s4+s5;
+avg=t/5;
+{
+printf("result=%d",t);
+printf("result=%d",avg);
+}
+}
+
